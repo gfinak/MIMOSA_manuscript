2ab9419b810a79d354d0b914153ab53fc170c30d
Greg Finak <gfinak@fhcrc.org>
2013-06-25 15:07:34 -0700

    Add code for generating manuscript figures

---
+++ MIMOSA/mimosafigures/compile.R
@@ -0,0 +1,5 @@
+library(knitr)
+library(knitrBootstrap)
+knitr::spin("manuscriptFigures.R",FALSE)
+knit_bootstrap("manuscriptFigures.Rmd")
+system("cp manuscriptFigures.html /home/gfinak/Dropbox/Public/MIMOSA/manuscriptFigures.html")
